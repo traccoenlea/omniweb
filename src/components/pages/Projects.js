f79b3ba44ef77e99e5e5fe08f698cf143ea5d66d
traccoenlea <leatraccoen@gmail.com>
2024-02-27 14:35:09 +0100

    starting the projects page skeleton

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,5 +1,50 @@
 import React from "react";
 
 export const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div className="appContainer">
+      <h1>Nos projets Communication</h1>
+      <h2>
+        Petite phrase trop chouette pour présenter les projets{" "}
+        <span className="color">Communication</span>.
+      </h2>
+
+      <div className="flex flexc ">
+        <div className="flex jcb">
+          <div
+            style={{
+              border: "1px solid purple",
+              width: "30%",
+              aspectRatio: "3/2",
+            }}
+          >
+            Petite div
+          </div>
+          <div style={{ border: "1px solid purple", width: "30%" }}>
+            Petite div
+          </div>{" "}
+          <div style={{ border: "1px solid purple", width: "30%" }}>
+            Petite div
+          </div>
+        </div>
+        <div className="flex jcb">
+          <div
+            style={{
+              border: "1px solid purple",
+              width: "30%",
+              height: "200px",
+            }}
+          >
+            Petite div
+          </div>
+          <div style={{ border: "1px solid purple", width: "30%" }}>
+            Petite div
+          </div>{" "}
+          <div style={{ border: "1px solid purple", width: "30%" }}>
+            Petite div
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
