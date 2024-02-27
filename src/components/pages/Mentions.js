aa6cf0816c9520f95999e4db96c74282ce65c0ca
traccoenlea <leatraccoen@gmail.com>
2024-02-27 11:44:41 +0100

    mentions' skeleton done

--- src/components/pages/Mentions.js
+++ src/components/pages/Mentions.js
@@ -1,5 +1,23 @@
 import React from "react";
 
 export const Mentions = () => {
-  return <div>Mentions</div>;
+  return (
+    <div className="appContainer mentionsContainer flex flexc jcc">
+      <h1>Mentions Légales</h1>
+      <p className="paragraph">
+        Jujubes chocolate cake brownie dessert toffee caramels bear claw oat
+        cake caramels. Ice cream donut bear claw caramels caramels. Jelly bear
+        claw marshmallow candy dessert jelly-o shortbread. Icing chocolate cake
+        cotton candy brownie danish fruitcake. Tart brownie jujubes brownie
+        danish.
+        <span className="color"> Biscuit jelly halvah sweet soufflé pie.</span>
+        Croissant sweet roll biscuit chocolate cake marzipan. Biscuit apple pie
+        gingerbread apple pie liquorice tart sugar plum cupcake gingerbread.
+        Danish apple pie cake cake gummies cake pastry. Candy sugar plum
+        croissant cupcake cookie danish ice cream gingerbread muffin. Brownie
+        ice cream shortbread candy chocolate gummies cookie. Tart chocolate bar
+        gummi bears bonbon donut dragée dessert.
+      </p>
+    </div>
+  );
 };
