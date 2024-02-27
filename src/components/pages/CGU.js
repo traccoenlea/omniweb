0319ee415435a499b59ee75be86f920e2e0de195
traccoenlea <leatraccoen@gmail.com>
2024-02-27 11:48:29 +0100

    cgu's skeleton done

--- src/components/pages/CGU.js
+++ src/components/pages/CGU.js
@@ -1,5 +1,23 @@
 import React from "react";
 
 export const CGU = () => {
-  return <div>CGU</div>;
+  return (
+    <div className="appContainer cguContainer flex flexc jcc">
+      <h1>Conditions Générales d'Utilisation</h1>
+      <p className="paragraph">
+        Jujubes chocolate cake brownie dessert toffee caramels bear claw oat
+        cake caramels. Ice cream donut bear claw caramels caramels. Jelly bear
+        claw marshmallow candy dessert jelly-o shortbread. Icing chocolate cake
+        cotton candy brownie danish fruitcake. Tart brownie jujubes brownie
+        danish.
+        <span className="color"> Biscuit jelly halvah sweet soufflé pie.</span>
+        Croissant sweet roll biscuit chocolate cake marzipan. Biscuit apple pie
+        gingerbread apple pie liquorice tart sugar plum cupcake gingerbread.
+        Danish apple pie cake cake gummies cake pastry. Candy sugar plum
+        croissant cupcake cookie danish ice cream gingerbread muffin. Brownie
+        ice cream shortbread candy chocolate gummies cookie. Tart chocolate bar
+        gummi bears bonbon donut dragée dessert.
+      </p>
+    </div>
+  );
 };
