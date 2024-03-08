d6d18f72502c7b14f46c6c9ae21b2895a11b0d10
traccoenlea <leatraccoen@gmail.com>
2024-03-08 12:34:04 +0100

    get data from agency story done

--- src/components/components/Chronologie.js
+++ src/components/components/Chronologie.js
@@ -1,62 +1,77 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ModalChronologie from "./ModalChronologie";
+import { getAgencyStory } from "../../apis/agency";
 
 export const Chronologie = () => {
   const [openModalIndex, setOpenModalIndex] = useState(null);
+
+  const [story, setStory] = useState([]);
 
   const handleClickModal = (index) => {
     setOpenModalIndex(index);
   };
 
-  let dataChrono = [
-    {
-      id: 1,
-      title: "Mars 2023",
-      text: "Mike et Tracco se rencontrent tavu. Lorem ipsum dolor sit amet, consectetur adipiscing elit Mike et Tracco se rencontrent tavu. Lorem ipsum dolor sit amet, consectetur adipiscing elit ",
-      left: true,
-      right: false,
-    },
-    {
-      id: 2,
-      title: "Juillet 2023",
-      text: "Flo, Mike et Léa parlent de créer une boite. gOn parle de Flo ? Lorem ipsum dolor sit amet, Flo, Mike et Léa parlent de créer une boite. gOn parle de Flo ? Lorem ipsum dolor sit amet, ",
-      left: false,
-      right: true,
-    },
-    {
-      id: 3,
-      title: "Octobre 2023",
-      text: "Examen passé, examen eu Lorem ipsum dolor sit amet, consectetur adipiscing elit Examen passé, examen eu Lorem ipsum dolor sit amet, consectetur adipiscing elit ",
-      left: true,
-      right: false,
-    },
-    {
-      id: 4,
-      title: "Novembre 2023",
-      text: "Direction la BGE et début de la formation CCPE Lorem ipsum dolor sit amet, Direction la BGE et début de la formation CCPE Lorem ipsum dolor sit amet, ",
-      left: false,
-      right: true,
-    },
-    {
-      id: 5,
-      title: "Février 2024",
-      text: "Exams sur exams, CCPE acquis (trouver un moyen de le mettre en avant sans dire que c'est 1 sur 2) + Entrée en incubatest Exams sur exams, CCPE acquis (trouver un moyen de le mettre en avant sans dire que c'est 1 sur 2) + Entrée en incubatest ",
-      left: true,
-      right: false,
-    },
-  ];
+  useEffect(() => {
+    async function getStory() {
+      try {
+        const data = await getAgencyStory();
+        setStory(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getStory();
+  }, []);
+
+  // let dataChrono = [
+  //   {
+  //     id: 1,
+  //     title: "Mars 2023",
+  //     text: "Mike et Tracco se rencontrent tavu. Lorem ipsum dolor sit amet, consectetur adipiscing elit Mike et Tracco se rencontrent tavu. Lorem ipsum dolor sit amet, consectetur adipiscing elit ",
+  //     left: true,
+  //     right: false,
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "Juillet 2023",
+  //     text: "Flo, Mike et Léa parlent de créer une boite. gOn parle de Flo ? Lorem ipsum dolor sit amet, Flo, Mike et Léa parlent de créer une boite. gOn parle de Flo ? Lorem ipsum dolor sit amet, ",
+  //     left: false,
+  //     right: true,
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "Octobre 2023",
+  //     text: "Examen passé, examen eu Lorem ipsum dolor sit amet, consectetur adipiscing elit Examen passé, examen eu Lorem ipsum dolor sit amet, consectetur adipiscing elit ",
+  //     left: true,
+  //     right: false,
+  //   },
+  //   {
+  //     id: 4,
+  //     title: "Novembre 2023",
+  //     text: "Direction la BGE et début de la formation CCPE Lorem ipsum dolor sit amet, Direction la BGE et début de la formation CCPE Lorem ipsum dolor sit amet, ",
+  //     left: false,
+  //     right: true,
+  //   },
+  //   {
+  //     id: 5,
+  //     title: "Février 2024",
+  //     text: "Exams sur exams, CCPE acquis (trouver un moyen de le mettre en avant sans dire que c'est 1 sur 2) + Entrée en incubatest Exams sur exams, CCPE acquis (trouver un moyen de le mettre en avant sans dire que c'est 1 sur 2) + Entrée en incubatest ",
+  //     left: true,
+  //     right: false,
+  //   },
+  // ];
 
   return (
     <div className="agencyContainer ">
-      {dataChrono.map((d, i) => (
+      {story.map((d, i) => (
         <div key={i} className="flex stepContainer">
-          {d.left ? (
+          {d.id % 2 === 1 ? (
             <div className="stepContainer flex">
               <div className="stepText">
                 <div className="TitleDateContainer">
-                  <h2 className="TitleDate">{d.title}</h2>
+                  <h2 className="TitleDate">{d.date}</h2>
                 </div>
-                <p className="TextDate">{d.text}</p>
+                <p className="TextDate">{d.resume}</p>
                 <button
                   className="ButtonChronoModal"
                   onClick={() => handleClickModal(i)}
@@ -67,11 +82,11 @@ export const Chronologie = () => {
 
               <div className="middleContainer flex jcc">
                 <div
-                  className={`${d.left ? "topBorder" : ""} blankSpace`}
+                  className={`${d.id % 2 === 1 ? "topBorder" : ""} blankSpace`}
                 ></div>
                 <div className="verticalBorder"></div>
                 <div
-                  className={`${d.right ? "topBorder" : ""} blankSpace`}
+                  className={`${d.id % 2 === 0 ? "topBorder" : ""} blankSpace`}
                 ></div>
               </div>
               <div className="stepNumber wideText flex jcfs">{d.id}</div>
@@ -81,19 +96,19 @@ export const Chronologie = () => {
               <div className="stepNumber wideText flex jcfs">{d.id}</div>
               <div className="middleContainer flex jcc">
                 <div
-                  className={`${d.left ? "topBorder" : ""} blankSpace`}
+                  className={`${d.id % 2 === 1 ? "topBorder" : ""} blankSpace`}
                 ></div>
                 <div className="verticalBorder"></div>
                 <div
-                  className={`${d.right ? "topBorder" : ""} blankSpace`}
+                  className={`${d.id % 2 === 0 ? "topBorder" : ""} blankSpace`}
                 ></div>
               </div>
 
               <div className="stepText">
                 <div className="TitleDateContainer">
-                  <h2 className="TitleDate">{d.title}</h2>
+                  <h2 className="TitleDate">{d.date}</h2>
                 </div>
-                <p className="TextDate">{d.text}</p>
+                <p className="TextDate">{d.resume}</p>
                 <button
                   className="ButtonChronoModal"
                   onClick={() => handleClickModal(i)}
