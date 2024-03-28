1f9a24183ae53cd6616b4c4b31387df72bd0059c
traccoenlea <leatraccoen@gmail.com>
2024-03-28 13:27:20 +0100

    blog page desktop ok without pagination

--- src/components/components/ArticleContainer.js
+++ src/components/components/ArticleContainer.js
@@ -1,10 +1,14 @@
 import React from "react";
 
 const ArticleContainer = ({ title, text, img }) => {
+  // on récup title, text et img
+  // on découpe le texte à 70car et le title à 18 pour qu'il passe sur une ligne
   let shortText = text.slice(0, 70);
   let shortTitle = title.slice(0, 18);
   return (
+    //container de chaque article
     <div className="articleContainer flex flexc">
+      {/* si y'a une image dans la db on la charge, sinon on charge l'image par défaut */}
       {img ? (
         <img
           src={img}
@@ -19,15 +23,19 @@ const ArticleContainer = ({ title, text, img }) => {
         />
       )}
 
+      {/* ici tout le container du text */}
       <div className="textContainer flex flexc jcb">
         <div>
+          {/* si le titre = au titre écourté on met le titre */}
           {shortTitle === title ? (
             <h3 className="articleTitle">{title}</h3>
           ) : (
+            // sinon on met le titre écourté avec ...
             <h3 className="articleTitle">{shortTitle}...</h3>
           )}
         </div>
         <div>
+          {/* ici le début de l'article avec pts de susp */}
           <p>{shortText}...</p>
         </div>
         <div>
--- src/components/pages/Blog.js
+++ src/components/pages/Blog.js
@@ -5,22 +5,12 @@ import { getArticles } from "../../apis/articles";
 export const Blog = () => {
   const [articles, setArticles] = useState([]);
 
-  const [page, setPage] = useState(1);
-
-  const [maxPage, setMaxPage] = useState(0);
-
-  const [pagination, setPagination] = useState([]);
-
   useEffect(() => {
     async function getListOfArticles() {
       try {
         const data = await getArticles();
 
         setArticles(data);
-
-        console.log(data.length);
-        setMaxPage(Math.ceil(data.length / 6));
-        return "ok";
       } catch (error) {
         console.error(error);
       }
@@ -29,64 +19,23 @@ export const Blog = () => {
     getListOfArticles();
   }, []);
 
-  if (page === 1 && articles.length !== 0 && pagination.length === 0) {
-    console.log("on passe là");
-    for (let x = 0; x <= 5; x++) {
-      // console.log(articles[x]);
-      let newArticle = articles[x];
-      setPagination((pagination) => [...pagination, newArticle]);
-    }
-  }
-
-  // console.log("pagination : " + pagination);
-
-  function handleNext() {
-    console.log("on arrive après");
-    if (page + 1 <= maxPage) {
-      console.log("c possible");
-      setPage(page + 1);
-    }
-  }
-
-  function handlePrevious() {
-    console.log("on arrive avant");
-    if (page !== 1 && page !== 0) {
-      setPage(page - 1);
-    }
-  }
-
   return (
     <div className="appContainer">
       <div className="TitleContainerAccent mt60">
         <h1 className="Title">Notre blog</h1>
       </div>
-      <div className="flex flexf jcb">
-        {pagination.map((a, i) => (
-          <div key={i} className="oneArticle">
-            <ArticleContainer title={a.title} text={a.text} img={a.img} />
-          </div>
-        ))}
-      </div>
-      <br />
-      <div className="flex">
-        {page === 1 ? (
-          <div>
-            <button onClick={handleNext}>Next</button>
-          </div>
-        ) : (
-          <div className="flex ">
-            <div>
-              <button onClick={handlePrevious}>Previous</button>
+
+      {articles.length === 0 ? (
+        <h2>Les articles arrivent...</h2>
+      ) : (
+        <div className="flex flexf jcb">
+          {articles.map((p, i) => (
+            <div key={i} className="oneArticle">
+              <ArticleContainer title={p.title} text={p.text} img={p.img} />
             </div>
-            <div>
-              <button onClick={handleNext}>Next</button>
-            </div>
-          </div>
-        )}
-        <div>
-          Page {page} sur {maxPage}
+          ))}
         </div>
-      </div>
+      )}
     </div>
   );
 };
