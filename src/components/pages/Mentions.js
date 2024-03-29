5fc2d0cf6c4b0496c656cd68de6a65decf4950c7
michaelvb-24 <michael.vanbouchotte1@gmail.com>
2024-03-29 10:49:56 +0100

    mentions done 2

--- src/components/pages/Mentions.js
+++ src/components/pages/Mentions.js
@@ -2,25 +2,30 @@ import React from "react";
 
 export const Mentions = () => {
   return (
-    <div className="appContainer mentionsContainer flex flexc jcc">
-      <div className="TitleContainerAccent">
-        <h1 className="Title">Mentions Légales</h1>
-      </div>
+    <div className="appContainer flex flexc jcc">
+      <div className="mentionsContainer">
+        <div className="TitleContainerAccent">
+          <h1 className="Title">Mentions Légales</h1>
+        </div>
 
-      <p className="paragraph">
-        Jujubes chocolate cake brownie dessert toffee caramels bear claw oat
-        cake caramels. Ice cream donut bear claw caramels caramels. Jelly bear
-        claw marshmallow candy dessert jelly-o shortbread. Icing chocolate cake
-        cotton candy brownie danish fruitcake. Tart brownie jujubes brownie
-        danish.
-        <span className="color"> Biscuit jelly halvah sweet soufflé pie.</span>
-        Croissant sweet roll biscuit chocolate cake marzipan. Biscuit apple pie
-        gingerbread apple pie liquorice tart sugar plum cupcake gingerbread.
-        Danish apple pie cake cake gummies cake pastry. Candy sugar plum
-        croissant cupcake cookie danish ice cream gingerbread muffin. Brownie
-        ice cream shortbread candy chocolate gummies cookie. Tart chocolate bar
-        gummi bears bonbon donut dragée dessert.
-      </p>
+        <p className="paragraph">
+          Jujubes chocolate cake brownie dessert toffee caramels bear claw oat
+          cake caramels. Ice cream donut bear claw caramels caramels. Jelly bear
+          claw marshmallow candy dessert jelly-o shortbread. Icing chocolate
+          cake cotton candy brownie danish fruitcake. Tart brownie jujubes
+          brownie danish.
+          <span className="color">
+            {" "}
+            Biscuit jelly halvah sweet soufflé pie.
+          </span>
+          Croissant sweet roll biscuit chocolate cake marzipan. Biscuit apple
+          pie gingerbread apple pie liquorice tart sugar plum cupcake
+          gingerbread. Danish apple pie cake cake gummies cake pastry. Candy
+          sugar plum croissant cupcake cookie danish ice cream gingerbread
+          muffin. Brownie ice cream shortbread candy chocolate gummies cookie.
+          Tart chocolate bar gummi bears bonbon donut dragée dessert.
+        </p>
+      </div>
     </div>
   );
 };
