b52cd1ba941218af0ac3444460f1ce8831b960de
traccoenlea <leatraccoen@gmail.com>
2024-04-11 11:14:19 +0200

    cgu and mentions done

--- src/components/pages/CGU.js
+++ src/components/pages/CGU.js
@@ -2,22 +2,42 @@ import React from "react";
 
 export const CGU = () => {
   return (
-    <div className="appContainer cguContainer flex flexc jcc">
-      <h1>Conditions Générales d'Utilisation</h1>
-      <p className="paragraph">
-        Jujubes chocolate cake brownie dessert toffee caramels bear claw oat
-        cake caramels. Ice cream donut bear claw caramels caramels. Jelly bear
-        claw marshmallow candy dessert jelly-o shortbread. Icing chocolate cake
-        cotton candy brownie danish fruitcake. Tart brownie jujubes brownie
-        danish.
-        <span className="color"> Biscuit jelly halvah sweet soufflé pie.</span>
-        Croissant sweet roll biscuit chocolate cake marzipan. Biscuit apple pie
-        gingerbread apple pie liquorice tart sugar plum cupcake gingerbread.
-        Danish apple pie cake cake gummies cake pastry. Candy sugar plum
-        croissant cupcake cookie danish ice cream gingerbread muffin. Brownie
-        ice cream shortbread candy chocolate gummies cookie. Tart chocolate bar
-        gummi bears bonbon donut dragée dessert.
-      </p>
+    <div className="appContainer flex flexc jcc">
+      <div className="mentionsContainer">
+        <div className="TitleContainerAccent">
+          <h1 className="Title">Conditions Générales d'Utilisation</h1>
+        </div>
+
+        <p>
+          En conformité avec le règlement n°2016/679, dit Règlement général pour
+          la protection des données (RGPD) et la loi n°78-17 du 6 janvier 1978
+          modifiée relative à l'informatique, aux fichiers et aux libertés, les
+          données à caractère personnel recueillies sur le site de l’entreprise
+          font l’objet d’une déclaration auprès du Délégué à la Protection des
+          données (DPO) de l’entreprise. Elles sont exclusivement à usage
+          interne de l’entreprise. Les données personnelles collectées par
+          l’entreprise peuvent l’être pour différentes finalités, selon les
+          pages du site et les formulaires de recueil concernés. Principalement,
+          il s’agit de la gestion des demandes d’informations. Les données sont
+          conservées pendant toute la durée de la relation établie, et jusqu’à 3
+          ans maximum après, selon la nature de la relation concernée. Les
+          données ainsi collectées ne seront ni cédées, ni échangées ou louées.
+          Conformément aux textes susvisés, vous disposez d'un droit
+          d’opposition, d'accès, de rectification, de suppression, de limitation
+          ou de portabilité portant sur les données vous concernant. Ces droits
+          peuvent être exercés en vous adressant à : Léa TRACCOEN. Correspondant
+          Informatique et libertés, au 07 49 41 48 42 en précisant dans l’objet
+          du courrier « Droit des personnes » et en joignant la copie de votre
+          justificatif d’identité :
+          <br />
+          soit directement sur le site, par courriel :
+          <span className="color"> contact@omnistos.fr </span>
+          <br />
+          soit en écrivant au :{" "}
+          <span className="color">4 rue des Buisses, 59800 Lille</span>
+          <br />
+        </p>
+      </div>
     </div>
   );
 };
--- src/components/pages/Mentions.js
+++ src/components/pages/Mentions.js
@@ -7,7 +7,6 @@ export const Mentions = () => {
         <div className="TitleContainerAccent">
           <h1 className="Title">Mentions Légales</h1>
         </div>
-
         <ul>
           <li>Nom, Prénom du Directeur de publication : Léa TRACCOEN</li>
           <li>
@@ -31,36 +30,6 @@ export const Mentions = () => {
           </li>
           <li>Représentant légal : Michaël Van Bouchotte</li>
         </ul>
-
-        <p>
-          En conformité avec le règlement n°2016/679, dit Règlement général pour
-          la protection des données (RGPD) et la loi n°78-17 du 6 janvier 1978
-          modifiée relative à l'informatique, aux fichiers et aux libertés, les
-          données à caractère personnel recueillies sur le site de l’entreprise
-          font l’objet d’une déclaration auprès du Délégué à la Protection des
-          données (DPO) de l’entreprise. Elles sont exclusivement à usage
-          interne de l’entreprise. Les données personnelles collectées par
-          l’entreprise peuvent l’être pour différentes finalités, selon les
-          pages du site et les formulaires de recueil concernés. Principalement,
-          il s’agit de la gestion des demandes d’informations. Les données sont
-          conservées pendant toute la durée de la relation établie, et jusqu’à 3
-          ans maximum après, selon la nature de la relation concernée. Les
-          données ainsi collectées ne seront ni cédées, ni échangées ou louées.
-          Conformément aux textes susvisés, vous disposez d'un droit
-          d’opposition, d'accès, de rectification, de suppression, de limitation
-          ou de portabilité portant sur les données vous concernant. Ces droits
-          peuvent être exercés en vous adressant à : Léa TRACCOEN. Correspondant
-          Informatique et libertés, au 07 49 41 48 42 en précisant dans l’objet
-          du courrier « Droit des personnes » et en joignant la copie de votre
-          justificatif d’identité :
-          <br />
-          soit directement sur le site, par courriel :
-          <span className="color"> contact@omnistos.fr </span>
-          <br />
-          soit en écrivant au :{" "}
-          <span className="color">4 rue des Buisses, 59800 Lille</span>
-          <br />
-        </p>
       </div>
     </div>
   );
