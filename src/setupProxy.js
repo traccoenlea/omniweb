2838c402b46dff0bdfb54bfa9674a4e038f3c565
traccoenlea <leatraccoen@gmail.com>
2024-04-18 16:10:53 +0200

    change proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,8 +4,8 @@ module.exports = function (app) {
   app.use(
     "/api/*",
     createProxyMiddleware({
-      // target: "http://127.0.0.1:8000",
-      target: "https://omniweback.vercel.app/",
+      target: "http://127.0.0.1:8000",
+      // target: "https://omniweback.vercel.app/",
       secure: false,
       changeOrigin: true,
     })
