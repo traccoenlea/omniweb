3616078d83777478688e2d7ab90c991da730d99f
traccoenlea <leatraccoen@gmail.com>
2024-04-18 11:26:37 +0200

    changed the proxy target

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,8 @@ module.exports = function (app) {
   app.use(
     "/api/*",
     createProxyMiddleware({
-      target: "http://127.0.0.1:8000",
+      // target: "http://127.0.0.1:8000",
+      target: "https://omniweback.vercel.app/",
       secure: false,
     })
   );
